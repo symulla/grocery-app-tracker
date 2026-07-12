3cc45069e0b8090b7b87f39877a43c474fad329c
symulla <mullasylvia2.0@gmail.com>
2026-07-12 19:38:07 +0300

    Configure GitHub Pages base path and router basename

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './assets/styles.css';
 
+const basename = import.meta.env.PROD ? '/grocery-app-tracker/' : '/';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
